513f27796121493417287146598de5f126f3200d
Veronica <verokeyty@gmail.com>
2020-03-30 23:45:21 -0600

    ActualizacionBD

--- db/db.sql
+++ db/db.sql
@@ -2,22 +2,40 @@ create database Proyecto;
 use Proyecto;
 
 
-create table tbAgricultores (
-idAgricultor int primary key not null auto_increment,
-nombAgricultor varchar(35),
-cedula varchar(15),
-provincia varchar(15),
-correo varchar(20),
-password varchar(255),
-direccion varchar(500),
-telefono varchar (20));
+create table tbUsuarios (
+idUsuario int primary key not null auto_increment,
+nombUsuario varchar(35),
+nombreCliente varchar(35),
+contrasena varchar(255));
 
 create table tbProductos(
 idProducto int primary key not null auto_increment,
 nombProducto varchar(40),
-cantidad varchar (220),
-img varchar (300),
-idAgricultor int not null,
-foreing key (idAgricultor) references tbAgricultores(idAgricultor)
+cantidad int (220),
+precio decimal(10,2), 
+img varchar (300));
 
-);
+
+create table tbCompra(
+	idFactura int primary key not null auto_increment, 
+	hora timestamp, 
+	total decimal(10,2) ,
+	idUsuario int not null,
+	foreign key (idUsuario) references tbUsuarios(idUsuario)); 
+
+
+ create table tbCompraLineas(
+ 	idFactura int not null,
+ 	cantidadTotal int,
+    idProducto int not null,
+    primary key(idFactura,idProducto),
+    foreign key (idFactura) references tbCompra(idFactura),
+    foreign key (idProducto) references tbProductos(idProducto));
+
+ create table tbCarrito(
+ 	cantidadTotal int,
+    idProducto int not null,
+    idUsuario int not null,
+    primary key(idProducto,idUsuario),
+    foreign key (idUsuario) references tbUsuarios(idUsuario),
+    foreign key (idProducto) references tbProductos(idProducto));
